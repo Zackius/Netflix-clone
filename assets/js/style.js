58aed5081664b37a623778325d34d0687dc67ecd
Zacharia <ndunguzachary24@gmail.com>
2022-07-06 00:54:11 +0300

    photos features updates

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,12 +1,7 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '687575473dmshf93ce8692bb11cep1dec44jsn45613d6339c8',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
-};
 
-fetch('https://moviesdatabase.p.rapidapi.com/titles/search/title/%7Btitle%7D?info=mini_info&limit=10&page=1&titleType=movie', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const url = 'https://api.themoviedb.org/3/movie/550?api_key=85a5fc8bbb74f8090b40918baf86bf98'
+
+fetch(url)
+	.then(resp = resp.json())
+	.then(data = data())
+	
